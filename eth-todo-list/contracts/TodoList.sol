aaa9561d968f735a21be64c48f2a524602872762
Nadani Dixon <nadani@Nadanis-MacBook-Pro.local>
2022-03-23 18:39:18 -0500

    initial setup and TodoList.sol added

---
+++ eth-todo-list/contracts/TodoList.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+contract TodoList {
+
+    // taskCount scope: entire smart contract 
+    uint public taskCount = 0; //state variable written to storage on the blockchain - changes smart contract state
+
+    //public creates a taskCount () that allows accesss to variable outside of smart contract 
+}
+    
